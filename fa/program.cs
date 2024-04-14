657ebaea71fba6ceb844525799a9464fa8dfef6a
g-g-gerin <124514292+g-g-gerin@users.noreply.github.com>
2024-04-14 12:50:03 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -14,29 +14,107 @@ namespace fans
   }
 
 
-  public class FA1
-  {
-    public bool? Run(IEnumerable<char> s)
+public class FA1
+{
+    private class State
     {
-      return false;
+        public Dictionary<char, State> Transitions = new Dictionary<char, State>();
+        public bool IsAcceptState;
     }
-  }
 
-  public class FA2
-  {
-    public bool? Run(IEnumerable<char> s)
+    private State start = new State { IsAcceptState = false };
+    private State oneZero = new State { IsAcceptState = false };
+    private State oneZeroOne = new State { IsAcceptState = true };
+
+    public FA1()
     {
-      return false;
+        start.Transitions['0'] = oneZero;
+        start.Transitions['1'] = start;
+        oneZero.Transitions['1'] = oneZeroOne;
     }
-  }
+
+    public bool? Run(string s)
+    {
+        State current = start;
+        foreach (char c in s)
+        {
+            if (!current.Transitions.TryGetValue(c, out current))
+                return null;
+        }
+        return current.IsAcceptState;
+    }
+}
+
+public class FA2
+{
+    private class State
+    {
+        public Dictionary<char, State> Transitions = new Dictionary<char, State>();
+        public bool IsAcceptState;
+    }
+
+    private State start = new State();
+    private State oddZero = new State();
+    private State oddOne = new State();
+    private State oddBoth = new State { IsAcceptState = true };
+
+    public FA2()
+    {
+        start.Transitions['0'] = oddZero;
+        start.Transitions['1'] = oddOne;
+        oddZero.Transitions['0'] = start;
+        oddZero.Transitions['1'] = oddBoth;
+        oddOne.Transitions['0'] = oddBoth;
+        oddOne.Transitions['1'] = start;
+        oddBoth.Transitions['0'] = oddOne;
+        oddBoth.Transitions['1'] = oddZero;
+    }
+
+    public bool? Run(string s)
+    {
+        State current = start;
+        foreach (char c in s)
+        {
+            if (!current.Transitions.TryGetValue(c, out current))
+                return null;
+        }
+        return current.IsAcceptState;
+    }
+}
   
-  public class FA3
-  {
-    public bool? Run(IEnumerable<char> s)
+public class FA3
+{
+    private class State
     {
-      return false;
+        public Dictionary<char, State> Transitions = new Dictionary<char, State>();
+        public bool IsAcceptState;
     }
-  }
+
+    private State start = new State();
+    private State one = new State();
+    private State twoOnes = new State { IsAcceptState = true };
+
+    public FA3()
+    {
+        start.Transitions['0'] = start;
+        start.Transitions['1'] = one;
+        one.Transitions['0'] = start;
+        one.Transitions['1'] = twoOnes;
+        twoOnes.Transitions['0'] = twoOnes;
+        twoOnes.Transitions['1'] = twoOnes;
+    }
+
+    public bool? Run(string s)
+    {
+        State current = start;
+        foreach (char c in s)
+        {
+            if (!current.Transitions.TryGetValue(c, out current))
+                return null;
+        }
+        return current.IsAcceptState;
+    }
+}
 
   class Program
   {
